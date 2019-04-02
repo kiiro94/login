9856b5d99888c09e60950ee2dce73c86403c9bfc
kiiro <albertobonalumi00@gmail.com>
2019-04-02 14:29:40 +0200

    table creation on first login

--- README.md
+++ README.md
@@ -10,13 +10,15 @@ Simple login page with pure PHP.
  1. Paste the files into the web folder you want to protect with a login
     page. In the source is provided an example index.php for reference.
  2. Create a new table called Users in your database, with columns like this:
-
-
-    | id  | username | password             |
-    |-----|----------|----------------------|
-    | int | varchar  | varchar (size >= 64) |
-
-
  3. Change the file `db_conn_settings.json` with the credentials of your Mysql server.
  4. Each page that wants to check if the user is  logged in has to have `checkLogin.php` imported.
- 5. Use `register.php` as a form action to register new users.
+ 5. A table called `Users` will be automatically created, with a template like this:
+
+
+    | id  | username | password          |
+    |-----|----------|-------------------|
+    | int | varchar  | varchar (size=64) |
+
+
+ 6. On the first login, the page will ask you to register the first user.
+ 7. Use `register.php` as a form action to register new users.
--- login.php
+++ login.php
@@ -4,14 +4,39 @@
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <link rel="stylesheet" href="loginStyle.css">
     <script> </script> 
+    <?php include "dbconn.php" ?>
+    <?php
+      //Check if table exists and create it if it doesn't
+      $res = $conn->query("SHOW TABLES LIKE 'Users'");
+      if ($res->num_rows === 0) {
+        $sql = 'CREATE TABLE Users (
+          id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+          username VARCHAR(20) NOT NULL UNIQUE,
+          password VARCHAR(64) NOT NULL,
+          registered_datetime DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP)';
+
+        if ($conn->query($sql) === TRUE) {
+          //
+        }
+        else {
+          echo "Error creating table: " . $conn->error;
+        }
+      }
+
+      //Check if the table is empty, if it's not, register new user
+      $res = $conn->query("SELECT COUNT(*) AS count from Users");
+      $data = $res->fetch_assoc();
+      $empty = true;
+      if ($data["count"] > 0) { $empty = false; }
+    ?>
   </head>
 
   <body>
-    <form class="login" action="logging.php" method="post" autocomplete="off">
+  <form class="login" action="<?php if ($empty) { echo "register.php"; } else { echo "logging.php"; } ?>" method="post" autocomplete="off">
       <input type="text" name="user" placeholder="username">
       <input type="password" name="pass" placeholder="password">
       <br>
-      <button type="submit">login</button>
+      <button type="submit"><?php if ($empty) { echo "register first user"; } else { echo "login"; }?></button>
     </form>
 
     <?php
